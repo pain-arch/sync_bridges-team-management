e727dad270b9863e54fb025e812c0adea2c4e238
pain-arch <himulol123@gmail.com>
2024-12-11 20:48:20 +0600

    Add welcome message and button to Home page; refactor next.config.mjs for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div className="">
-
+      <h1 className="">Welcome to Next.js!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: "https",
+//                 hostname: "example.com",
+//             }
+//         ]
+//     }
+// };
+
+// import withNextIntl from 'next-intl/plugin';
+
+// export default withNextIntl("./i18n.ts")(nextConfig);
+
+
+
+// Import the Next.js Internationalization plugin
+import withNextIntl from "next-intl/plugin";
+
+// Define the Next.js configuration
 const nextConfig = {};
 
-import withNextIntl from 'next-intl/plugin';
-
+// Export the configuration wrapped with the Internationalization plugin
 export default withNextIntl("./i18n.ts")(nextConfig);
